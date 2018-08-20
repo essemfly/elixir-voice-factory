e7f5e83ae77ebd534246738cd536ad4a009aeb5d
seokmin <seokmin@chain.partners>
2018-08-20 13:54:10 +0900

    Implement antd design and layout page with first route

---
+++ assets/src/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="landing">
+    <h3>안녕 친구들?</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "landing",
+  data() {
+    return {};
+  },
+  async created() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ assets/src/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="test">
+    <h3>안녕 테스트 친구들?</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {};
+  },
+  async created() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+</style>
